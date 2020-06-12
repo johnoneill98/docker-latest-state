3c615d5699ef3a155cfe95582cf4bc0b302b0baf
John O'Neill <joneil15@students.towson.edu>
2020-06-12 11:54:34 -0400

    Added eroor handeling and optimized string manipulation
    Signed-off-by: John O'Neill <joneil15@students.towson.edu>

--- api.py
+++ api.py
@@ -3,15 +3,21 @@ import json
 import requests
 
 def search(ui):
-    url = 'https://hub.docker.com/v2/repositories/library/'
+    url = "https://hub.docker.com/v2/repositories/library/"
+    ui = ui.replace(' ','-' )
     response = requests.get(url+ui+'/')
-    data = json.loads(response.text)
-    desc = data['full_description']
-    line  = desc.split('\n')
-    for text in line:
-        if 'latest' in text:
-            left = '[`'
-            right = '`,'
-            version = text[text.index(left)+len(left):text.index(right)]
-            print ("Version is: "+version)
-            break
+    try:
+        data = json.loads(response.text)
+        desc = data['full_description']
+        lDesc = desc.split('\n')
+        lDesc = list(filter(lambda x: 'latest' in x,lDesc))
+        lDesc = lDesc[0].split(',')
+        sDesc =(lDesc[0].split('`')[1].split(lDesc[0])[0])
+        sDesc2 =(int(sDesc[0]))
+        print(sDesc)
+
+#  Error Handling
+    except KeyError as err:
+        print("Can not find image")
+    except ValueError as err:
+        print("Can not find the latest version of the image")
