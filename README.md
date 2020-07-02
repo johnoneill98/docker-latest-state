73b78199aa6100207bdcba67f8ce11ce4a0b3326
John O'Neill <joneil15@students.towson.edu>
2020-07-02 12:13:22 -0400

    modified code after code review
    Signed-off-by: John O'Neill <joneil15@students.towson.edu>

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ https://hub.docker.com/v2/repositories/library/?page=1&page_size=100
 To use the `docker.py` the user has to format the command line arguments as such
 
 ```
-python3 docker.py <command>
+python3 docker.py <image_name>
 ```
 The reason why it is python3 instead of python is because `docker.py` uses the urllib.request module that is not supported by python.
 
@@ -24,59 +24,56 @@ The reason why it is python3 instead of python is because `docker.py` uses the u
 
 ### The Latest Version Command
 ```
-$python3 docker.py -l <image-name>
+$python3 docker.py <image-name> -l
 ```
-This commands lets the user to get the information specific to the image they have entered.
-
-It will return the whatever version was latest for that image
+This commands lets the user to see whatever version was the latest for that image
 ### The Last Updated  command
 ~~~
-python3 docker.py -i <image-name>
+python3 docker.py <image-name>
 ~~~
 This commands allows the user top see what eas the last updated version. Most people would think that the latest version is the last updated but that is not the case for some images in DockerHub's registry:
 
 Example
 ```
-$python3 docker.py -l postgres
+$ python3 docker.py postgres -l
+The version of postgres that was last updated is 9.6.18
 The latest version of postgres is 13
-$python3 docker.py -i postgres
-Last updated version of postgres is 9.6.18
 ```
 ### The Search Version Command
 ```
-python3 docker.py -l <image-name> -v <version-number>
+python3 docker.py <image_name> -v <version_number>
 ```
 This command allows the user to search to see if the version of an image exists with Docker Hub
 If the Version doesn't exist, the user will be prompted with all the current versions of the image and after that the latest version of the image
 Example:
 ```
-$ python3 docker.py -l mongo -v 24
-Version: ['24'] is not a valid version of  mongo
+$ python3 docker.py mongo -v 24
+The version of mongo that was last updated is 4.2.8
+Version 24 is not a valid version of  mongo
 Valid versions of mongo are
-['4.0.17', '4.2.5', '3.6.17', '4.2.6', '4.0.18', '4.2.7', '3', '3.6', '3.6.18', '4', '4.0', '4.0.19', '4.2', '4.2.8']
-The latest version of mongo is 4.2.8
+['3', '3.6', '3.6.17', '3.6.18', '4', '4.0', '4.0.18', '4.0.19', '4.2', '4.2.5', '4.2.6', '4.2.7', '4.2.8']
+
 ```
-In order to find the versions the user must put `-l <image-name>` before teh version number
 
 #### The help command
-If the user needs help with teh commands and doesn't want to return to this page, use this command
+If the user needs help with the commands and doesn't want to return to this page, use this command
 ```
-$ python3 docker.py -h
-usage: docker.py [-h] [--lastupdated, IMAGE] [--version VERSION]
-                 [--latestversion LATEST]
 
-This script takes a image and either shows the latest version of the image or
-if the version is a valid version
+$ python3 docker.py
+usage: docker.py [-h] [-v VERSION] [-l] image
+
+This script takes a image and shows either the the latest version, the last
+version that was updated of the image or if the version is a valid version
+
+positional arguments:
+  image       please enter a image name like so 'python3 docker.py
+              <imagename>'
 
 optional arguments:
-  -h, --help            show this help message and exit
-  --lastupdated, IMAGE, -i IMAGE
-                        Please enter an image to get the last updated version
-  --version VERSION, -v VERSION
-                        Please enter a image then its version number
-  --latestversion LATEST, -l LATEST
-                        Please enter an image to get the lastest version`
-
+  -h, --help  show this help message and exit
+  -v VERSION  Please enter a image then its version number like so 'python3
+              docker.py <imagename> -v <versionnumber>'
+  -l          Please enter '-l' to get the latest version like so 'python3
+              docker.py <imagename> -l'
 ```
-
 
--- docker.py
+++ docker.py
@@ -1,9 +1,11 @@
-
-import argparse
-import requests
+"import statements"
 from operator import itemgetter
+import argparse
 import sys
-def validVersion(vname):
+import requests
+
+
+def validversion(vname):
     """[summary]
     This function takes the version number of an images and tells if its a valid version or not.
     If the version has words in ti then it is is valid
@@ -12,124 +14,150 @@ def validVersion(vname):
         vname: the name of the version
 
     Returns:
-        A boolean value that tells if teh version number is valid or not based on
+        A boolean value that tells if the version number is valid or not based on
         if the version numbers has only numbers and decimals or not
     """
-    isvalid= True
-    for s in vname:
-        if not (s.isnumeric() or s == '.'):
+    isvalid = True
+    for value in vname:
+        if not (value.isnumeric() or value == "."):
             isvalid = False
             break
     return isvalid
 
+
 def api(image):
     """[summary]
-    User sumbits an image name and it will contact the api endpoint to retrive all the version that docker supports
+    User sumbits an image name and it will contact the api endpoint to retrive all the version
+    information that docker supports
     Args:
         image from dockerHub
 
     Returns:
         a list of all version of the supported  images from dockerHub
     """
-    #replaces all spaces with dashes to satisfy the api call
-    ui = image.replace(" ", "-")
+    # replaces all spaces with dashes to satisfy the api call
+    user = image.replace(" ", "-")
     # complete api call
-    if ui.find("/") != -1:
+    if user.find("/") != -1:
         url = "https://registry.hub.docker.com/v2/repositories/"
     else:
         url = "https://registry.hub.docker.com/v2/repositories/library/"
-    response = requests.get(f"{url}/{ui}/tags?page=1&page_size=100")
+    response = requests.get(f"{url}/{user}/tags?page=1&page_size=100")
     # Error checking
     if response.status_code != 200:
         print(f"Error {image} does not have any infomarion about version numbers")
-        quit()
+        sys.exit()
     response = response.json()
     if response["results"] == []:
-        print(f"Error{ui}does not hae any information about version numbers")
+        print(f"Error{user}does not hae any information about version numbers")
     response = response["results"]
     return response
-def manipulate(response) :
-    names ={ }
-    test =[]
-    for l in response:
-        vname =l["name"]
-        last_updated=l["last_updated"]
+
+
+def manipulate(response):
+    """[summary]
+    Takes the input from the api call and manipulates it into a readable list
+
+    Args:
+        response ([list]): a list of all version for a image
+
+    Raises:
+        IndexError: If the user puts an incorrect image
+
+    Returns:
+        [list] returns all the images by earliest updated to latest updated
+    """
+    names = {}
+    test = []
+    for las in response:
+        vname = las["name"]
+        last_updated = las["last_updated"]
         test.append(last_updated)
-        if last_updated == None:
+        if last_updated is None:
             test.remove(last_updated)
         # formating the date for it to be easier to read
         last_updated = str(test).replace("T", " ")
-        if validVersion(vname):
-            values={last_updated:vname}
+        if validversion(vname):
+            values = {last_updated: vname}
             names.update(values)
     # list of tuples from earliest and latest
-    new_list=(sorted(names.items(), key = lambda kv: (kv[0], kv[1])))
-    #itemgetter takes the list of tuples  and returns every value at index 1 for each tuple
+    new_list = sorted(names.items(), key=lambda kv: (kv[1], kv[0]))
+    # itemgetter takes the list of tuples  and returns every value at index 1 for each tuple
     new_list = list(map(itemgetter(1), new_list))
     if new_list == []:
-        raise IndexError(f" image does not exist")
+        raise IndexError("image does not exist")
     return new_list
-def last_updated(image, update):
-    # finds out what is the latest version of an image
-    print(f"Last updated version of {image} is {update[-1]}")
 
-def version_list(name, image, number):
-    # see if a version is valid or not, if it isn't it wil give a lists of valid versions
-    if number not in image:
-         print(f"Version: {number} is not a valid version of  {name}")
-         print(f"Valid versions of {name} are")
-         print(f"{image}")
-    else:
-        print(f"Version {number} is a valid version of {name}")
-def last_version(image,versions):
-    """[summary]
-    Takes the list of valid version numbers and conversts it to a number in order to sort for lowest to highest
-    Args:
-        image ([type]): [a valid version name]
+
+def last_version(versions):
     """
-    finallist =[]
-    for v in versions:
-        versionNumberList = v.split(".")
-        y = 0.0
-        for i,n in enumerate(versionNumberList):
-            y = y+(int(n)/(10**i))
-        finallist.append((y,v))
-    finallist.sort( reverse = True)
+    Retrives a list of tuples and sorts the versiona from highest to lowest
 
-   # printing(image,finallist[0][1])
-    print(f"The latest version of {image} is {finallist[0][1]}")
+    Args:
+        versions ([list of tuples]): a list tuples from the manipulate function
 
-def printing(a,b):
-    if last_version:
-        print(f"The latest version of {a} is {b}")
+    Returns:
+        [str]: a string that is the latest version of an image
+    """
+    finallist = []
+    for newversion in versions:
+        versionnumberlist = newversion.split(".")
+        actualnumber = 0.0
+        for iterate, newplace in enumerate(versionnumberlist):
+            actualnumber = actualnumber + (int(newplace) / (10 ** iterate))
+        finallist.append((actualnumber, newversion))
+    finallist.sort(reverse=True)
+    return finallist[0][1]
+
 
 def main():
+    """
+    The function will the read the command the arguments and will do the appropriate action
+    """
     parser = argparse.ArgumentParser(
-        description='This script takes a image and either shows the latest version of the image or if the version is a valid version'
+        description="This script takes a image and shows either the "
+        "the latest version, the last version that was updated of the image"
+        " or if the version is a valid version"
     )
-    parser.add_argument('--lastupdated,','-i', dest='image', type=str,
-                        help= "Please enter an image to get the last updated version")
-    parser.add_argument('--version','-v', action='append', dest='version', type=str,
-                        help= "Please enter a image then its version number ")
-    parser.add_argument('--latestversion','-l', dest='latest', type=str,
-                        help= "Please enter an image to get the lastest version ")
-
-    args = parser.parse_args()
+    parser.add_argument(
+        "image",
+        type=str,
+        help="please enter a image name like so 'python3 docker.py <imagename>'",
+    )
+    parser.add_argument(
+        "-v",
+        action="append",
+        dest="version",
+        type=str,
+        help="Please enter a image then its version number like so"
+        " 'python3 docker.py <imagename> -v <versionnumber>'",
+    )
+    parser.add_argument(
+        "-l",
+        action="store_true",
+        help="Please enter '-l' to get the latest version like so "
+        " 'python3 docker.py <imagename> -l'",
+    )
     if len(sys.argv) < 2:
         parser.print_help()
         sys.exit(1)
-    if args.image:
-        versions = api(args.image)
-        number = manipulate(versions)
-        last_updated(args.image, number)
+    args = parser.parse_args()
+    versions = api(args.image)
+    number = manipulate(versions)
+    print(f"The version of {args.image} that was last updated is {number[-1]}")
     if args.version:
-        versions = api(args.latest)
-        info = manipulate(versions)
-        version_list(args.latest, info, args.version)
-    if args.latest:
-        versions = api(args.latest)
-        number = manipulate (versions)
-        last_version(args.latest,number)
-if __name__ == '__main__':
-    main()
+        value = args.version.pop()
+        if value not in number:
+            print(f"Version: {value} is not a valid version of  {args.image}")
+            print(f"Valid versions of {args.image} are")
+            print(f"{number}")
+        else:
+            print(f"Version {value} is a valid version of {args.image}")
 
+    if args.l:
+        latest = last_version(number)
+        print(f"The latest version of {args.image} is {latest}")
+
+
+if __name__ == "__main__":
+    main()
